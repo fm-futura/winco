66a028e881c5893b6ec6f2a5fe4f9b06335a86e9
Adrian Pardini <github@tangopardo.com.ar>
2017-03-16 22:28:01 -0300

    Grilla: fetch and parse data from our wp api

--- app/main/services/grilla-serv.js
+++ app/main/services/grilla-serv.js
@@ -2,21 +2,78 @@
 angular.module('main')
 .service('Grilla', function (Restangular, Config) {
   var state = {
-    data: undefined
+    rawData: undefined,
+    // weekDay -> list of shows. Sunday is 0
+    daysData: {
+    },
   };
 
   var Service = {
     get: get,
     getCurrentShow: getCurrentShow,
   };
+
+  initDaysData(state.daysData);
   return Service;
+
+  function initDaysData (daysData) {
+    for (var day = 0; day < 7; day++) {
+      daysData[day] = [];
+    }
+  }
+
+  function parseData (data) {
+    var daysData = state.daysData;
+
+    _.each(daysData, function (d) {
+      d.length = 0;
+    });
+
+    _.each(data, function (showData) {
+      var weekStart = moment().startOf('week');
+      var start = parseTime(showData.horario_inicio);
+      var end = parseTime(showData.horario_finalizacion);
+
+      if (end.hour === 0 && end.minutes === 0) {
+        end.hour = 23;
+        end.minutes = 59;
+      }
+
+      _.each(showData.dia, function (day) {
+        // maps 1 - 7 to 1 - 0
+        day = day % 7;
+
+        var dateStart = moment(weekStart).add(day, 'days').set(start);
+        var dateEnd   = moment(weekStart).add(day, 'days').set(end);
+
+        var show = {
+          start: dateStart,
+          end: dateEnd,
+          title: showData.title.rendered,
+          url: showData.link,
+        };
+
+        daysData[day].push(show);
+      });
+    });
+
+    function parseTime (time) {
+      var parts = time.split(':');
+      return {
+        hour: parseInt(parts[0]),
+        minutes: parseInt(parts[1]),
+        seconds: 0,
+      };
+    }
+  }
 
   function get (options) {
 
     var p = Restangular.all(Config.ENV.schedule).getList(options);
 
     p.then(function (data) {
-      state.data = data;
+      state.rawData = data;
+      parseData(data);
     });
 
     return p;
@@ -24,8 +81,14 @@ angular.module('main')
 
   /* eslint no-unused-vars:warn */
   function getCurrentShow (when) {
-    when = when || new Date();
-    return {};
+    when = moment(when);
+    var day = when.weekday();
+
+    var show  = _.find(state.daysData[day], function (show) {
+      return when.isBetween(show.start, show.end);
+    });
+
+    return show;
   }
 
 });
